7aa2e2d853afd04482b032f62ce9e1300b05bf45
Swapnil Opash <sra.opash@gmail.com>
2024-01-25 18:48:04 +0530

    fixed

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -106,7 +106,9 @@ exports.notificationMail = async (userData) => {
 
 exports.channelNotificationEmail = async (userData) => {
   let name = userData?.Username;
-  let msg = `you are assigned in HT channel by MasterAdmin`;
+  let msg = `You have been assign in InsuranceAgent channel by the InsuranceAgent Admin.
+  To access your channel, log into your InsuranceAgent account,click on the
+  InsuranceAgent icon at the top of the page,then click on My Channel.`;
   let redirectUrl = `${environment.FRONTEND_URL}`;
 
   const mailObj = {
@@ -131,9 +133,9 @@ exports.communityApproveEmail = async (profileId, isApprove) => {
       userData[0]?.FirstName + " " + userData[0]?.LastName;
     let msg = "";
     if (isApprove === "Y") {
-      msg = `Your Insurance Agency has been approved by Master Admin.`;
+      msg = `InsuranceAgent has approved your Insurance Agency.`;
     } else {
-      msg = `Your Insurance Agency has been unapproved by Master Admin.`;
+      msg = `InsuranceAgent has unapproved your Insurance Agency.`;
     }
     let redirectUrl = `${environment.FRONTEND_URL}`;
     const mailObj = {
